fc0815d155b1ad2d05d255c983dd4b662297c6d6
Denis <dean.angell@mail.ru>
2021-05-05 17:18:48 +0300

    refactor: change method name in pets-slider

--- online-zoo/pages/common/js/modules/slider/pets-slider.js
+++ online-zoo/pages/common/js/modules/slider/pets-slider.js
@@ -24,7 +24,7 @@ export default class PetsSlider extends Slider{
         }
     }
 
-    hundleState(n) {
+    handlerState(n) {
         switch (n) {
             case 'begin':
                 this.state = [1,2,3,4];
@@ -60,19 +60,18 @@ export default class PetsSlider extends Slider{
         this.slides.forEach(slide=>{
             slide.classList.remove(this.activeClass);
         });
-
         this.slides[this.slideIndex-1].classList.add(this.activeClass);
     }
 
     nextSlide(){
         if (this.slideIndex == this.state[this.size - 1] && this.slideIndex == this.slides.length) {
-            this.hundleState('begin');
+            this.handlerState('begin');
             this.state = [1,2,3,4];
             this.offset = 0;
             this.slideIndex = 1;
             this.range.value = 1;
         } else if (this.slideIndex == this.state[this.size - 1]) {
-            this.hundleState('next');
+            this.handlerState('next');
             this.slideIndex++;
             this.range.value++;
             this.offset += (100 / this.slides.length);
@@ -87,12 +86,12 @@ export default class PetsSlider extends Slider{
    
     prevSlide(){
         if (this.slideIndex == this.state[0] && this.slideIndex == 1) {
-            this.hundleState('end');
+            this.handlerState('end');
             this.slideIndex = this.slides.length;
             this.range.value = this.slides.length;
             this.offset = (100 / this.slides.length) * (this.slideIndex - this.size); 
         } else if(this.slideIndex == this.state[0]) {
-            this.hundleState('prev');
+            this.handlerState('prev');
             this.slideIndex--; 
             this.range.value--;
             this.offset -= (100 / this.slides.length);
@@ -110,12 +109,12 @@ export default class PetsSlider extends Slider{
         let inputValue = this.slideIndex;
         this.slideIndex = this.range.value;
         if ((inputValue == this.state[this.size - 1]) && (this.slideIndex > inputValue)) {
-            this.hundleState('next');
+            this.handlerState('next');
             this.offset += (100 / this.slides.length);
             this.slidesField.style.transform = `translateX(-${this.offset}%)`;
         }
         if (((inputValue == this.state[0])) && (this.slideIndex < inputValue)) {
-            this.hundleState('prev');
+            this.handlerState('prev');
             this.offset -= (100 / this.slides.length);
             this.slidesField.style.transform = `translateX(-${this.offset}%)`;
         }
